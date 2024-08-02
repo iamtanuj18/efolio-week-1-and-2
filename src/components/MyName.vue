8816ce0fb112a5f3512550f795165b51c7e262bf
Tanuj <iamtanuj18@yahoo.com>
2024-08-02 23:38:29 +1000

    efolio completed for week 1 and 2

---
+++ src/components/MyName.vue
@@ -0,0 +1,25 @@
+<script>
+
+</script>
+
+<template>
+  <div  class="text-center">
+    <h3>
+      Welcome, TANUJ
+    </h3>
+  </div>
+</template>
+
+<style>
+
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.text-center h3 {
+    text-align: center;
+}
+
+
+</style>
